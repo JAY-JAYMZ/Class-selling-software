090f3734c11b971a5fe31acab9e9ec4fce02543d
JAY-JAYMZ <164818244+JAY-JAYMZ@users.noreply.github.com>
2024-06-06 14:02:27 +0800

    Add files via upload

---
+++ js/dengru.js
@@ -0,0 +1,88 @@
+			let users=[{name:'asd',pwd:'111'},{name:'王五',pwd:'111'},{name:'a',pwd:'111'}];
+			
+			
+			let username=document.getElementById("username");
+			let pwd=document.getElementById("pwd");
+			let btn=document.getElementById("log");
+			
+			/* 点击按钮后触发声明 */
+			btn.onclick=function(){
+				/* 遍历两个数组 */
+				let ret=users.some(function(value){
+					return value.name==username.value && value.pwd==pwd.value;
+				});
+				
+				
+				/* 判断键盘获取用户名和密码是否注册了并存储在了locaStorage中 */
+				if(username.value==localStorage.getItem('username1') && pwd.value==localStorage.getItem('pwd1')){
+					alert("User login successful");
+					window.location.href="index.html"
+							
+				}else if(ret){
+					alert("User login successful");
+					window.location.href="index.html"
+					
+				
+				}else{/* 输入错误 */
+					alert("Enter the correct username and password");
+				}
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function myFunction(){
+			var x = document.getElementById("demo").value;
+			var z=document.getElementById("name").value;
+			var y = document.getElementById("damo");
+			
+			/*console.log(z);*/
+				/*alert('恭喜发财')*/
+			/*if(x==""&&z==""){
+					alert("表格内不能为空");
+						}
+			else{
+				if(x=="123456"&&z=="my"){
+				
+					y.href = "图片查看/project.html";
+				
+					}
+					else if(x==""&&z=="my"){
+	
+						alert("您的密码不能为空");
+					
+					}
+					else if(x=="123456"&&z==""){
+	
+						alert("您的用户名不能为空");
+					
+					}
+				else{
+					alert("您填写的用户名或密码有误，请在输入一遍")
+				}
+			}
+			
+		} */
---
+++ js/zhuce.js
@@ -0,0 +1,26 @@
+		
+			let username1=document.getElementById("username");
+			let phone1=document.getElementById("phone");
+			let pwd1=document.getElementById("pwd");
+			let pwd12=document.getElementById("pwd2");
+			let btn1=document.getElementById("log");
+			
+				/* 点击按钮后触发 */
+			btn1.onclick=function(){
+				/* 判断是否有没填写的信息 */
+				if(username.value==""||(phone.value=="")||(pwd.value=="")||pwd2.value==""){
+					alert("Please fill in the information completely");
+				}/* 判断两次密码是否不一致 */
+				else if(pwd1.value==pwd12.value){ 
+					/* 存储用户名和密码 */
+					localStorage.setItem('username1',username.value);
+					localStorage.setItem('pwd1',pwd.value);
+					/* 弹出提示并跳转页面 */
+					alert("User registration successful\nPlease login");
+					window.location.href="dengru.html";
+					window.event.returnValue=false;
+					}else{
+						alert(" Two password content is inconsistent!\nPlease re-enter");
+					}
+				
+			} 
